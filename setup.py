382f99197a918c05e3f886409ab091ccf2078740
Ravi Kumar <ravikumar46931@gmail.com>
2024-02-01 22:01:50 +0530

    setup file updated

--- setup.py
+++ setup.py
@@ -27,4 +27,5 @@ setup(
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     # Install requires may be added at here
+    install_requires=['pymongo', 'pymongo[srv]', 'dnspython', 'pandas', 'numpy', 'ensure', 'pytest', 'termcolor', 'mysql-connector-python', 'prettytable'],
     )
